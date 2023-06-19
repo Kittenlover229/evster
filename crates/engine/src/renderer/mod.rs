11391f42190a6a1e6e67fdb6937031c348076d27
Kittenlover229 <artur.roos.artur@gmail.com>
2023-06-19 21:12:21 +0300

    Restructure the renderer

--- crates/engine/src/renderer/mod.rs
+++ crates/engine/src/renderer/mod.rs
@@ -1,33 +1,8 @@
-use bytemuck::{Pod, Zeroable};
 use wgpu::util::DeviceExt;
 use winit::{event::WindowEvent, window::Window};
 
-#[repr(C)]
-#[derive(Copy, Clone, Debug, Pod, Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-}
-
-impl Vertex {
-    fn layout() -> wgpu::VertexBufferLayout<'static> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-            ],
-        }
-    }
-}
+mod vertex;
+use vertex::Vertex;
 
 const VERTICES: &[Vertex] = &[
     Vertex {
