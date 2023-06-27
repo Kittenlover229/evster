cd51c6b32398c1e762893b982d51140a0755bf66
Kittenlover229 <artur.roos.artur@gmail.com>
2023-06-27 18:01:54 +0300

    Add WebGL support

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use nalgebra_glm::{I8Vec2, Vec2, Vec3};
+use nalgebra_glm::{Vec2, Vec3};
 use winit::{
     dpi::PhysicalPosition,
     event::*,
@@ -8,8 +8,11 @@ use winit::{
     window::WindowBuilder,
 };
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 use engine::{
-    Action, Actor, ActorTemplate, Atlas, AxialInput2D, FrameBuilder, Grid, InputHandler, Instance,
+    Actor, ActorTemplate, Atlas, AxialInput2D, FrameBuilder, Grid, InputHandler, Instance,
     Renderer, Tile, World,
 };
 
@@ -50,8 +53,22 @@ pub fn frame_from_world<'a>(
 // Palette:
 // https://lospec.com/palette-list/2bit-demichrome
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    main().unwrap()
+}
+
+
 pub fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        } else {
+            env_logger::init();
+        }
+    }
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("Evster")
@@ -76,6 +93,25 @@ pub fn main() -> anyhow::Result<()> {
         }]
         .into_iter(),
     );
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        // Winit prevents sizing with CSS, so we have to set
+        // the size manually when on web.
+        use winit::dpi::PhysicalSize;
+        window.set_inner_size(PhysicalSize::new(450, 400));
+
+        use winit::platform::web::WindowExtWebSys;
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| {
+                let dst = doc.get_element_by_id("evster-attach-here")?;
+                let canvas = web_sys::Element::from(window.canvas());
+                dst.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("Couldn't append canvas to document body.");
+    }
 
     let snek = ActorTemplate::new("Snek", "monster.snek");
     let snek = Rc::new(snek);
