c3abfae8a07b22a3be54df373d486c25af76d30d
Kittenlover229 <artur.roos.artur@gmail.com>
2023-06-27 18:21:59 +0300

    Prefer pretty_env_logger

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -65,7 +65,7 @@ pub fn main() -> anyhow::Result<()> {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
             console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
         } else {
-            env_logger::init();
+            pretty_env_logger::init();
         }
     }
 
